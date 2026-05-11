fc6ed6ad28eca7a9442d98980774d2a6ba5ce251
CelulaAgape <celulaagapeie@gmail.com>
2026-05-11 13:05:18 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzMl9OJKxAclv5v-ynFD7bs-RazJn9zx0",
+  authDomain: "escala-celula.firebaseapp.com",
+  projectId: "escala-celula",
+  storageBucket: "escala-celula.firebasestorage.app",
+  messagingSenderId: "948233776560",
+  appId: "1:948233776560:web:a836f11a09513b4966fa75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
